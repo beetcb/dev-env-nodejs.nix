720111df51980a4ca3436c056fba2836f07a0562
alexphzhou <alexphzhou@tencent.com>
2022-09-17 17:03:35 +0800

    write env dynamically

--- flake.nix
+++ flake.nix
@@ -7,13 +7,29 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    # support all the platforms that are supported by nix.
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        envRaw = ''
+          # add node npm pkgs to $PATH.
+          layout node
+          # load reproducible dev environment declaration.
+          use flake . --impure
+        '';
+      in
       rec {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ nodejs-14_x ];
+          buildInputs = with pkgs; [
+            nodejs-14_x
+            nix-direnv
+          ];
 
           shellHook = ''
+            node --version
+            echo "${envRaw}" >> .env &&
+            direnv allow . &&
+            npm i
           '';
         };
       }
